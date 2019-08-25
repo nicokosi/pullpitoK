dfde01f5aa99e48602139e3f1491199e9569b7b1
Nicolas Kosinski <nicokosi@yahoo.com>
2019-08-25 17:45:16 +0200

    ⬆ Upgrade dependencies to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.41"
-    id("se.patrikerdes.use-latest-versions") version "0.2.11"
-    id("com.github.ben-manes.versions") version "0.21.0"
+    id("org.jetbrains.kotlin.jvm") version "1.3.50"
+    id("se.patrikerdes.use-latest-versions") version "0.2.12"
+    id("com.github.ben-manes.versions") version "0.22.0"
     id("com.github.johnrengelman.shadow") version "5.1.0"
     id("com.adarshr.test-logger") version "1.7.0"
     application
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    compile("com.fasterxml.jackson.core:jackson-databind:2.9.9")
+    compile("com.fasterxml.jackson.core:jackson-databind:2.9.9.1")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
