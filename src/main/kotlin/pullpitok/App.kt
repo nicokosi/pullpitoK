nicokosi/pullpitoK

commit 88da1f925104ea596362d02a08731de62428c527

🎨 Clean code: remove IDEA and Sonar warnings and fix typos

src/main/kotlin/pullpitok/App.kt
@@ -4,11 +4,12 @@ import pullpitok.github.Action
 import pullpitok.github.Event
 import pullpitok.github.EventClient
 import pullpitok.github.Type
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     loadLibSunec()
 
-    if (!checkArgs(args)) System.exit(0)
+    if (!checkArgs(args)) exitProcess(0)
     val repo = args[0]
     val token = args.getOrNull(1) ?: ""
 
@@ -24,7 +25,7 @@ fun main(args: Array<String>) {
 
 private fun loadLibSunec() {
     val libSunec = System.getenv("PULLPITOK_LIBSUNEC")
-    if (libSunec != null && !libSunec.isEmpty()) {
+    if (libSunec != null && libSunec.isNotEmpty()) {
         System.setProperty("java.library.path", libSunec)
     } else {
         System.setProperty("java.library.path", System.getenv("JAVA_HOME"))
@@ -46,9 +47,9 @@ A command line tool to display a summary of GitHub pull requests.
 
 private fun displayEvents(repo: String, allEvents: MutableList<Event>) {
     val eventsPerAuthor = perAuthor(allEvents)
-    val opened: (Event) -> Boolean = { it.type == Type.PullRequestEvent.name && it.payload.action == Action.opened.name }
-    val commented: (Event) -> Boolean = { it.type == Type.PullRequestReviewCommentEvent.name && it.payload.action == Action.created.name }
-    val closed: (Event) -> Boolean = { it.type == Type.PullRequestEvent.name && it.payload.action == Action.closed.name }
+    val opened: (Event) -> Boolean = { it.type == Type.PullRequestEvent.name && it.payload.action == Action.Opened.name }
+    val commented: (Event) -> Boolean = { it.type == Type.PullRequestReviewCommentEvent.name && it.payload.action == Action.Created.name }
+    val closed: (Event) -> Boolean = { it.type == Type.PullRequestEvent.name && it.payload.action == Action.Closed.name }
     println("""pull requests for "$repo" ->
         opened per author ${counters(eventsPerAuthor, opened)}
         commented per author ${counters(eventsPerAuthor, commented)}

src/main/kotlin/pullpitok/github/Event.kt
@@ -4,12 +4,11 @@ data class Actor(val login: String)
 data class Payload(val action: String)
 data class Event(val id: String, val type: String, val actor: Actor, val payload: Payload)
 enum class Action {
-    created,
-    closed,
-    opened
+    Created,
+    Closed,
+    Opened
 }
 enum class Type {
-    IssueCommentEvent,
     PullRequestEvent,
     PullRequestReviewCommentEvent
 }

src/test/kotlin/pullpitok/AppTest.kt
@@ -14,15 +14,15 @@ class AppTest {
     fun `no counters`() {
         assertEquals(
                 "",
-                counters(emptyMap(), { true }));
+                counters(emptyMap()) { true })
     }
 
     @Test
     fun `one counter`() {
+        val event = Event(id = "1", type = "PullRequestEvent", actor = Actor("alice"), payload = Payload("opened"))
         assertEquals(
                 "\n\t\tauthor: 1",
-                counters(mapOf("author" to listOf(Event(id = "1", type = "PullRequestEvent", actor = Actor("alice"), payload = Payload("opened")))),
-                        { true }))
+                counters(mapOf("author" to listOf(event))) { true })
     }
 
     @Test

src/test/kotlin/pullpitok/github/EventClientTest.kt
@@ -3,7 +3,7 @@ package pullpitok.github
 import org.junit.Test
 import kotlin.test.assertEquals
 
-class GithubEventTest {
+class EventClientTest {
 
     @Test
     fun `can return no events with a mocked GitHub API`() {
@@ -24,9 +24,9 @@ class GithubEventTest {
                         Event(id = "3.1", type = "PullRequestReviewCommentEvent", actor = Actor("carol"), payload = Payload("created")),
                         Event(id = "3.2", type = "PullRequestReviewCommentEvent", actor = Actor("bob"), payload = Payload("created")),
                         Event(id = "5", type = "PullRequestEvent", actor = Actor("eve"), payload = Payload("closed"))),
-                EventClient().events(loadFile("/events.json")))
+                EventClient().events(loadJsonEventFile()))
     }
 
-    private fun loadFile(path: String) = GithubEventTest::class.java.getResource(path).readText()
+    private fun loadJsonEventFile() = EventClientTest::class.java.getResource("/events.json").readText()
 
 }

commit 5ac95fed6e2a2d4fd02a8703f86a859e58710aa7

🐛 Fix "missing counters" bug introduced in commit 88da1f9 😇
For instance, the following command

	./gradlew run --args "nicokosi/pullpitoK"

now displays:

	pull requests for "nicokosi/pullpitoK" ->
	        opened per author
	                nicokosi: 5
	        commented per author
	        closed per author
	                nicokosi: 5

instead of:

	pull requests for "nicokosi/pullpitoK" ->
	        opened per author
	        commented per author
	        closed per author

src/main/kotlin/pullpitok/App.kt
@@ -47,9 +47,9 @@ A command line tool to display a summary of GitHub pull requests.
 
 private fun displayEvents(repo: String, allEvents: MutableList<Event>) {
     val eventsPerAuthor = perAuthor(allEvents)
-    val opened: (Event) -> Boolean = { it.type == Type.PullRequestEvent.name && it.payload.action == Action.Opened.name }
-    val commented: (Event) -> Boolean = { it.type == Type.PullRequestReviewCommentEvent.name && it.payload.action == Action.Created.name }
-    val closed: (Event) -> Boolean = { it.type == Type.PullRequestEvent.name && it.payload.action == Action.Closed.name }
+    val opened: (Event) -> Boolean = { it.type == Type.PullRequestEvent.name && it.payload.action == Action.opened.name }
+    val commented: (Event) -> Boolean = { it.type == Type.PullRequestReviewCommentEvent.name && it.payload.action == Action.created.name }
+    val closed: (Event) -> Boolean = { it.type == Type.PullRequestEvent.name && it.payload.action == Action.closed.name }
     println("""pull requests for "$repo" ->
         opened per author ${counters(eventsPerAuthor, opened)}
         commented per author ${counters(eventsPerAuthor, commented)}

src/main/kotlin/pullpitok/github/Event.kt
@@ -4,9 +4,9 @@ data class Actor(val login: String)
 data class Payload(val action: String)
 data class Event(val id: String, val type: String, val actor: Actor, val payload: Payload)
 enum class Action {
-    Created,
-    Closed,
-    Opened
+    created,
+    closed,
+    opened
 }
 enum class Type {
     PullRequestEvent,