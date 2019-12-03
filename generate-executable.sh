7dae769e70a18cf8b2eb02ec6483de804066475b
Nicolas Kosinski <nicokosi@yahoo.com>
2019-12-03 07:05:44 +0100

    ⬆ Upgrade GraalVM to 19.3.0.r11
    GraalVM 19.3 is the first release of GraalVM based on JDK 11:
    https://www.graalvm.org/docs/release-notes/19_3/
    
    Moreover, we can now get rid of jre/lib/libsunec dependency:
    
    	Native Image
    
    	GraalVM 19.3 switched to using the JDK native code instead of manual substitutions. For GraalVM Native Image this switch to Java Native Interface (JNI) platform enabled the possibility to provide support for JDK 11 and extended support for Windows OS. It has no impact on startup time or memory footprint, and eliminates the need for shipping JDK libraries such as libsunec.so along with native images that use Java crypto services. GraalVM now ships with statically linkable version of the JDK libraries.

--- generate-executable.sh
+++ generate-executable.sh
@@ -10,22 +10,10 @@ echo "JAR file has been built! ✅"
 echo "Install GraalVM via SDKMAN!:"
 curl --silent "https://get.sdkman.io" | bash || echo 'SDKMAN! already installed'
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-GRAALVM_VERSION="19.2.1-grl"
+GRAALVM_VERSION="19.3.0.r11-grl"
 sdkman_auto_answer=true sdk install java $GRAALVM_VERSION > /dev/null || echo "GraalVM $GRAALVM_VERSION already installed."
 sdk use java $GRAALVM_VERSION
 
-echo "Copying 'libsunec' shared library (Sun Elliptic Curve crypto):"
-set +e
-cp $JAVA_HOME/jre/lib/*/libsunec* . 2>/dev/null # Linux
-linux_libsunec=$?
-cp $JAVA_HOME/jre/lib/libsunec* . 2>/dev/null #macOS
-macos_libsunec=$?
-set -e
-if [ $linux_libsunec -ne 0 ] && [ $macos_libsunec -ne 0 ]; then
-	echo "Cannot copy 'libsunec' shared library"
-	exit 1
-fi
-echo "'libsunec' shared library has been copied! ✅"
 echo "Build executable from JAR via GraalVM:"
 gu install native-image && \
 native-image \
