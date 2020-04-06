69adeb7d81aac93bbf2debc79c9bf82d44638550
Nicolas Kosinski <nicokosi@yahoo.com>
2020-04-06 07:10:47 +0200

    🐛 Fix issue #32: Use Authorization header instead of deprecated query parameter
    See https://developer.github.com/changes/2020-02-10-deprecating-auth-through-query-param/

--- src/main/kotlin/pullpitok/github/EventClient.kt
+++ src/main/kotlin/pullpitok/github/EventClient.kt
@@ -1,13 +1,29 @@
 package pullpitok.github
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import java.net.URL
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+import java.time.Duration
 
 class EventClient {
 
+    private val client = HttpClient.newBuilder().build()
+
     fun githubEvents(repo: String, token: String, page: Int): List<Event> {
-        val json = URL("https://api.github.com/repos/$repo/events?access_token=$token&page=$page").readText()
-        return events(json)
+        val url = "https://api.github.com/repos/$repo/events?page=$page"
+        val request = HttpRequest.newBuilder()
+                .timeout(Duration.ofSeconds(30))
+                .header("Content-Type", "application/json")
+                .header("Authorization", "token $token")
+                .uri(URI.create(url))
+                .build()
+        val response = client.send(request, BodyHandlers.ofString())
+        if (response.statusCode() != 200) {
+            fail("Status code is ${response.statusCode()} for $url")
+        }
+        return events(response.body())
     }
 
     fun events(json: String): List<Event> {
@@ -22,6 +38,10 @@ class EventClient {
                                     node?.get("payload")?.get("action")?.asText().orEmpty())
                     )
                 }
+    }
+
+    private fun fail(message: String): Nothing {
+        throw IllegalArgumentException(message)
     }
 
 }
