b264a8fbb638d1372a8d0dc3e72c42cc5ebdb4e7
Nicolas Kosinski <nicokosi@yahoo.com>
2020-04-06 07:10:47 +0200

    Patch Kotlin to 1.3.71

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.70"
+    id("org.jetbrains.kotlin.jvm") version "1.3.71"
     id("se.patrikerdes.use-latest-versions") version "0.2.13"
     id("com.github.ben-manes.versions") version "0.28.0"
     id("com.github.johnrengelman.shadow") version "5.2.0"
