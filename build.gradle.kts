62092d196cfdb21bc765a5356c17530fdf6a3897
Nicolas Kosinski <nicokosi@yahoo.com>
2020-06-06 15:46:47 +0200

    ⬆ Upgrade Kotlin and libraries to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.71"
-    id("se.patrikerdes.use-latest-versions") version "0.2.13"
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    id("se.patrikerdes.use-latest-versions") version "0.2.14"
     id("com.github.ben-manes.versions") version "0.28.0"
     id("com.github.johnrengelman.shadow") version "5.2.0"
     id("com.adarshr.test-logger") version "2.0.0"
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.10.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
